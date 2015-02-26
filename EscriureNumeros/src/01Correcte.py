978722bbaa5b614da3e15119b8df770eb4bb0ec6
TresLos <asistare7@gmail.com>
2015-02-26 07:14:46 +0100

    Primera forma de sumar complerta

--- EscriureNumeros/src/01Correcte.py
+++ EscriureNumeros/src/01Correcte.py
@@ -1,85 +1,12 @@
 """
-Pel meu nivell estetic, opino que hauria de ser [menys significatiu, mes significatiu]
-
-Sense haver efectuat canvis reals al codi, crec que es funcional i funciona correctament ;)
+Aqui teniu un programa que ha sortit a partir de lluitar molt, pero sobretot d'haver estat lluitant
+per un sumador. Realment no ha aparegut per aqui aquest programa, sino pel sumador jajaja
 """
 
-
-# Suposem una entrada correcta
-# Entrada: [valor major significat, menor significat], lletres concidents amb la base, per tenir el incrementador Decrementador operatius
-# Sortida: major, menor
-def sumador (B, a, b):
-	incr, dcr = icrementorDecrementor (B)
-	null = B[0]
-	o = []
-	carry = False
-	while a and b:
-		q, carry = simpleSum (incr, dcr, carry, a.pop (), b.pop (), null)
-		o += [q]
-	if b: a = b
-	while carry and a:
-		q, carry = simpleCarry (incr, a.pop (), null)
-		o += [q]
-	if carry: o += [B[1]]
-	o += a
-	o.reverse ()
-	return o
-
-def simpleSum (incr, dcr, carry, a, b, null):
-	boles = []
-	while a != null:
-		boles += ['o']
-		a = dcr[a]
-	while b != null:
-		boles += ['o']
-		b = dcr[b]
-	if carry: boles += ['o']
-	o, carry = null, False
-	while boles:
-		boles.pop ()
-		o = incr[o]
-		if o == null: carry = True
-	return o, carry
-
-def simpleCarry (incr, a, null):
-	a = incr[a]
-	if a == null: return a, True
-	return a, False
-"""
-Ens em adonat que no podem progressar actualment per aquesta via, perque ens falta un conversor de bases.
-
-Uix, ara veig un gran problema... Per tenir el conversor necessitem saber operar...
-Aixi que ara per ara suposarem que les entrades son correctes
-"""
-
-# la B es la base // Es un diccionari, perque a priori no puc saber quin es el seguent 'pot esser arbritari depenen de la base'
-def icrementorDecrementor (B):
-	incr, dcr = {B[-1]: B[0]}, {B[0]: B[-1]}
-	for i in range (1, len(B)):
-		f, b = B[i], B[i -1]
-		incr[b], dcr[f] = f, b
-	return incr, dcr
-
-# Creador de bases, "no necessita en realitat d'un catala correcte, nomes que siguin diferents"
-def generaBase (e):
-	o = []
-	for i in range (e):
-		o += [parser (str (i))]
-	return o
-
-"""
-Petits canvis efectuats
-Ara funciona sense digits, nomes amb lletres
-Ara esta corregit els problemes que tenia amb els espais
-
-Confiem que tot estigui correcte, la veritat he fet una comprovacio inferior a 300 elements...
-	D'aquesta manera poc puc assegurar el bon funcionament del parser
-"""
 def mini (a = 200, b = 300):
 	for i in range (a, b):
 		print (parser (str(i)))
-
-# La entrada ha de ser un string ex '1234007'
+		
 def parser (e):
 	o, q, t = ['zero', 'zero', 'zero'], 1, 0
 	for i in e[::-1]:
